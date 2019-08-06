6a9377c3ddb1ef33956accf8305f255cdabc64a6
Vasilev Anatoliy <toliankir@gmail.com>
2019-08-06 16:50:46 +0300

    Change redirect url, take id_token

--- index.js
+++ index.js
@@ -8,24 +8,25 @@ app.set("view engine", "ejs");
 
 const client_id = '168338020470-8bgidgkifihqeqvmj29q94m9gvug0qte.apps.googleusercontent.com';
 const client_secret = 'D1D_4ugkXdBFJM2Xy5BYF4Y0';
+const scope = 'https://www.googleapis.com/auth/analytics.readonly+https://www.googleapis.com/auth/userinfo.email';
 
 app.get('/', (req, res) => {
     res.render("index", {
         client_id: client_id,
+        scope: scope,
         callback_url: 'http://localhost:3000/google'
     });
-    // res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/google', (req, res) => {
     if (req.query.code) console.log(`User code: ${req.query.code}`);
-    res.send(`code ${req.query}`);
+    res.send(`code ${req.query.code}`);
     request.post('https://www.googleapis.com/oauth2/v4/token', {
         json: {
             code: req.query.code,
             client_id: client_id,
             client_secret: client_secret,
-            redirect_uri: 'http://localhost:3000',
+            redirect_uri: 'http://localhost:3000/google',
             grant_type: 'authorization_code'
         }
     }, (err, res, body) => {
@@ -34,6 +35,8 @@ app.get('/google', (req, res) => {
             return;
         }
         console.log(`Status code: ${res.statusCode}`);
+        console.log(`Access token: ${body.access_token}`);
+        console.log(`Refresh token: ${body.refresh_token}`);
         console.log(body);
     });
 });
